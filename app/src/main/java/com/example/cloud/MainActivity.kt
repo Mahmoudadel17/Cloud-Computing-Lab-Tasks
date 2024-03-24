83dea2ca8d9d587ee0601dc11c100e1ad85f4d97
Mahmoud Adel <mahmoudadel5556@gmail.com>
2024-03-24 08:21:12 +0200

    task 1

--- app/src/main/java/com/example/cloud/MainActivity.kt
+++ app/src/main/java/com/example/cloud/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.cloud
 
 import android.os.Bundle
-import android.view.animation.Animation
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -9,11 +8,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.cloud.ui.theme.CloudTheme
 
 class MainActivity : ComponentActivity() {
